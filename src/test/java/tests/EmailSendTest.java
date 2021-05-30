784ebb0f9ab7933e1cadc3ea1584fd82db6d74be
Salavat Nizamov <slwtk@mail.ru>
2021-05-30 07:24:05 +0300

    code cleanup

--- src/test/java/tests/EmailSendTest.java
+++ src/test/java/tests/EmailSendTest.java
@@ -13,16 +13,12 @@ public class EmailSendTest extends TestBase {
     @Severity(SeverityLevel.TRIVIAL)
     @Description("Тест аутентификации, вычисление количества писем, включающих в subject слова: 'Simbirsoft Тестовое задание',а также отправка письма с результатами")
     public void testLogInAndCountingMatchingEmailsThenSendingEmail() {
-        //navigate to gmail index page(with redirect to login)
         step("Переход на начальную страницу");
         app.getNavigationHelper().navigateToIndexPage();
-        //logIn
         step("Аутентификация");
         app.getLoginHelper().login();
-        //find matching messages
         step("Поиск сообщений по критерию");
         app.getInboxHelper().findMatchingMessages();
-        //message send
         step("Формирование и отправка количества подходящих писем");
         app.getMailSenderHelper().sendEmail(app.getInboxHelper().getMatchingMessages());
     }
